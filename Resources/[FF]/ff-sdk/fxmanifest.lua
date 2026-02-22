5791d8e2d79d97870968b17a2265519033032a6f
hlpdev <hunterpollock2006@gmail.com>
2026-02-21 19:54:12 -0500

    removed dep from fxmanifest

--- Resources/[FF]/ff-sdk/fxmanifest.lua
+++ Resources/[FF]/ff-sdk/fxmanifest.lua
@@ -7,7 +7,6 @@ files {
     "Microsoft.Bcl.AsyncInterfaces.dll",
     "Microsoft.Extensions.DependencyInjection.Abstractions.dll",
     "Microsoft.Extensions.DependencyInjection.dll",
-    "Microsoft.Extensions.Logging.Abstractions.dll",
     "System.Threading.Tasks.Extensions.dll"
 }
 
